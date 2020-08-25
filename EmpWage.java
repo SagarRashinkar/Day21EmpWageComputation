d08eea5ae44d60d88281f65bbe751ae59e0f68a6
Sagar <sagarrashinkar2@gmail.com>
2020-08-26 01:13:55 +0530

    Adding part time employee

--- EmpWage.java
+++ EmpWage.java
@@ -1,25 +1,26 @@
 class EmpWage{
 
+	public static final int isFullTime = 1;
+	public static final int isPartTime = 2;
+	public static final int empRatePerHour = 20;
+
 	public static void main(String[] args){
-	
-		int isFullTime = 1;
-		int empRatePerHour = 20;
 
 		int empWage = 0;
 		int workHour = 0;
 
 		int check = (int)(Math.random()*2)+1;
-		if(check == isFullTime){
 		
+		if(check == 1){
 			workHour = 8;
 		}
+		else if(check == 2){
+			workHour = 4;
+		}
 		else{
-		
 			workHour = 0;
 		}
 		empWage = workHour * empRatePerHour;
 		System.out.println("Emp wage: "+empWage);
-
 	}
 }
-
