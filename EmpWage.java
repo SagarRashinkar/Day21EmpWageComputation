07c845c86062d42c364c5dd7dfce46816a8528df
Sagar <sagarrashinkar2@gmail.com>
2020-08-26 01:26:27 +0530

    calculating emp wage for a month

--- EmpWage.java
+++ EmpWage.java
@@ -3,27 +3,35 @@ class EmpWage{
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
 	public static final int empRatePerHour = 20;
+	public static final int workDayPerMonth = 20;
 
 	public static void main(String[] args){
 
 		int empWage = 0;
 		int workHour = 0;
+		int totalEmpWage = 0;
 
-		int check = (int)(Math.random()*2)+1;
+		for(int i=0;i<workDayPerMonth;i++){
+
+			
+			int check = (int)(Math.random()*2)+1;
 		
-		switch(check){
+			switch(check){
 
-			case isFullTime:
-				workHour = 8;
-				break;
-			case isPartTime:
-				workHour = 4;
-				break;
-			default:
-				workHour = 0;
-				break;
+				case isFullTime:
+					workHour = 8;
+					break;
+				case isPartTime:
+					workHour = 4;
+					break;
+				default:
+					workHour = 0;
+					break;
+			}
+			empWage = workHour * empRatePerHour;
+			totalEmpWage += empWage;
+			System.out.println("Emp wage: "+empWage);
 		}
-		empWage = workHour * empRatePerHour;
-		System.out.println("Emp wage: "+empWage);
+		System.out.println("Employee wage for a month is:"+totalEmpWage);
 	}
 }
